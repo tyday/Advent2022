21f855a9da1abf730464a1a0c3acf43c5c463708
Ty Day <tyrday@gmail.com>
2022-12-10 15:22:38 -0500

    Day 10 part 1 and part 2

--- src/main/java/net/tylerday/AdventofCode/day10/CPU.java
+++ src/main/java/net/tylerday/AdventofCode/day10/CPU.java
@@ -1,34 +1,75 @@
 package net.tylerday.AdventofCode.day10;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 public class CPU {
-    int registerX = 0;
+    int registerX = 1;
     int clockCycle = 0;
     int c1 = 0;
     int c2 = 0;
-    int total = 0;
+    int total = 1;
+    int totalSignalStrength = 0;
+//    List<Integer> interestingCycles = new ArrayList<>(Arrays.asList(20, 60, 100, 140, 180, 220));
+    List<Integer> interestingCycles = new ArrayList<>(Arrays.asList(40,80,120,160,200,240));
     int pointer = 0;
     List<String> data;
 
     Stack<String> cpuStack = new Stack<>();
+
+    char[][] display = new char[6][41];
+    int currentRow = 0;
 
     public CPU(List <String> data) {
         this.data = data;
     }
 
     public void run(){
-        while(pointer < data.size()) {
+        while(pointer < data.size() || !this.cpuStack.isEmpty()) {
+            clockCycle ++;
+
+            if(interestingCycles.contains(clockCycle)){
+                this.totalSignalStrength += this.clockCycle * this.registerX;
+                System.out.printf("Cycle: %d * registerX: %d = %d\n", this.clockCycle, this.registerX, this.totalSignalStrength);
+                currentRow ++;
+
+            }
+
             if (this.cpuStack.empty()) {
                 getCommand();
             }
             runCommand(cpuStack.pop());
 
-            if(clockCycle%20==0){
+            addPixel();
 
+        }
+        System.out.printf("Cycle: %d registerX: %d\n", this.clockCycle, this.registerX);
+        System.out.println();
+        printDisplay();
+    }
+
+    private void printDisplay() {
+        for (char[] row :
+                display) {
+            for (int i = 0; i < row.length; i++) {
+                if(row[i] == '#'){
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
             }
+            System.out.println();
+        }
+    }
+
+    private void addPixel() {
+        int horizontalPlacement = clockCycle % 40;
+        if((registerX - 1 <= horizontalPlacement) && (horizontalPlacement <= registerX + 1)){
+//            if((registerX > -1) && (registerX < 41)) {
+                display[currentRow][horizontalPlacement] = '#';
+//            }
         }
     }
 
@@ -36,17 +77,19 @@ public class CPU {
         String command = data.get(pointer);
         pointer++;
         if(!command.startsWith("noop")){
+            cpuStack.add(command);
             cpuStack.add("noop");
+        } else {
+            cpuStack.add(command);
         }
-        cpuStack.add(command);
     }
 
 
     public void runCommand(String c){
         if(!c.startsWith("noop")){
             String[] command = c.split(" ");
-            total += Integer.parseInt(command[1]);
+            registerX += Integer.parseInt(command[1]);
+            System.out.printf("    %s | %s\n", this.clockCycle, c);
         }
-        clockCycle ++;
     }
 }
